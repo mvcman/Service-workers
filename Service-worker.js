8f486265b190aa7f8aabe9a7b4d9e5d0a83fa581
Mandar Waghe <waghemandar1712@gmail.com>
2019-01-15 14:13:40 +0530

    about.html added!

--- Service-worker.js
+++ Service-worker.js
@@ -1,8 +1,46 @@
+
+
+const cacheAssets = [
+  'index.html',
+  'main.js',
+  'style.js'
+];
 // To Install serviceWorker
-self.addEventListener('install', event => {
-  console.log("Sevice Worker in installed......!");
+self.addEventListener('install', e => {
+  console.log("Sevice Worker installed......!");
+
+  e.waitUntil(
+  caches.open('v1')
+    .then(cache => {
+      console.log('Service Worker: caching Files');
+      cache.addAll(cacheAssets);
+    })
+    .then(() => self.skipWaiting())
+    .catch(err => console.log(err))
+  );
 })
 // To activate serviceWorker
-self.addEventListener('install', event => {
-  console.log("Sevice Worker in Activated......!");
+self.addEventListener('activate', e => {
+  console.log("Sevice Worker is Activated......!");
+
+  //TO remove unwanted caches
+e.waitUntil(
+  caches.keys().then(cacheNames => {
+    return Promise.all(
+      cacheNames.map(cache => {
+        if(cache !== 'v1'){
+          console.log('Service Worker: Clearing Old Cache');
+          return caches.delete(cache);
+        }
+      })
+    )
+  })
+);
+})
+// call fetch Event
+self.addEventListener('fetch', e => {
+  console.log("Service Worker: Fetching...");
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  )
 })
